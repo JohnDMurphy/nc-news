a100f2f9383e231d3cd08c089840e05dd7fcee19
John Murphy <jd031988@gmail.com>
2022-04-21 12:03:22 +0100

    Implement getting a single article

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getNewsArticles } from '../utils/api';
+
 import { Article } from './Article';
 
 export const Home = () => {
